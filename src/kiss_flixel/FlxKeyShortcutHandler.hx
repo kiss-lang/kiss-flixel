913be1e6c8473059531172a9e9fbc2982abecc73
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-07-06 09:46:58 -0600

    FlxKeyShortcutHandler first pass at gamepad support

--- src/kiss_flixel/FlxKeyShortcutHandler.hx
+++ src/kiss_flixel/FlxKeyShortcutHandler.hx
@@ -7,6 +7,10 @@ import kiss.List;
 import kiss_tools.KeyShortcutHandler;
 import flixel.input.keyboard.FlxKey;
 import flixel.FlxG;
+import flixel.input.actions.FlxAction;
+import flixel.input.actions.FlxActionInput;
+import flixel.input.FlxInput;
+import flixel.input.gamepad.FlxGamepadInputID;
 
 @:build(kiss.Kiss.build())
 class FlxKeyShortcutHandler<T> extends KeyShortcutHandler<T> {}
