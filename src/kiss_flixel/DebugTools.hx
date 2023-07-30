61e71a1f2b7128f837af3f6d6f4af3dc7c03d008
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-07-30 12:05:24 -0600

    F1 to record with kiss_tools.OBSTools

---
+++ src/kiss_flixel/DebugTools.hx
@@ -0,0 +1,17 @@
+package kiss_flixel;
+
+import kiss.Prelude;
+
+import kiss_tools.OBSTools;
+
+import kiss_flixel.SimpleWindow;
+
+import flixel.input.keyboard.FlxKey;
+import flixel.FlxG;
+import flixel.input.actions.FlxAction;
+import flixel.input.actions.FlxActionManager;
+import flixel.input.actions.FlxActionInput;
+import flixel.input.FlxInput;
+
+@:build(kiss.Kiss.build())
+class DebugTools {}
