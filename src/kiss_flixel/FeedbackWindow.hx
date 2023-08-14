ffbf4a90ad79f11f1bf9b46b1df664fc1cb2901b
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-08-13 19:34:10 -0600

    feedback window talks to the server!

---
+++ src/kiss_flixel/FeedbackWindow.hx
@@ -0,0 +1,13 @@
+package kiss_flixel;
+
+import kiss.Prelude;
+import kiss.List;
+import flixel.util.FlxColor;
+import lime.net.HTTPRequest;
+import haxe.io.Bytes;
+import kiss_flixel.KissInputText;
+import flixel.FlxG;
+import sys.io.File;
+
+@:build(kiss.Kiss.build())
+class FeedbackWindow extends SimpleWindow {}
