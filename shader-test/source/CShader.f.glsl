b8874f8e0cb4b777b9cfc8e20bdf17de487833ef
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-01-27 15:24:39 -0700

    add an example uniform var in shader-test

--- shader-test/source/CShader.f.glsl
+++ shader-test/source/CShader.f.glsl
@@ -1,7 +1,9 @@
 #pragma header
 
+uniform bool invert = true;
+
 void main()
 {
     vec4 color = flixel_texture2D(bitmap, openfl_TextureCoordv);
-    gl_FragColor = vec4((1.0 - color.r) * color.a, (1.0 - color.g) * color.a, (1.0 - color.b) * color.a,   color.a);
+    gl_FragColor = invert ? vec4((1.0 - color.r) * color.a, (1.0 - color.g) * color.a, (1.0 - color.b) * color.a,   color.a) : color;
 }
